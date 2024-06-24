a89155bf3b25f3b7ed35f55db8158aaf9f8cb80d
lott-e <hi.lottie@icloud.com>
2024-06-24 20:19:12 +0100

    alien

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,14 +17,21 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
             Docusaurus Tutorial - 5min ⏱️
           </Link>
-        </div>
+        </div> */}
+        <iframe
+          src="https://giphy.com/embed/wqQptjdbYd0qDXOmxD"
+          width="480"
+          height="480"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
       </div>
     </header>
   );
